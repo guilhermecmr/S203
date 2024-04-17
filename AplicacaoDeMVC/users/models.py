0567e139025aa129ab47420fac44672d1e99c56f
Guilherme Cotta Marques Ribeiro <guilhermecottamr@gmail.com>
2024-04-16 22:23:39 -0300

    Atualizações

--- AplicacaoDeMVC/users/models.py
+++ AplicacaoDeMVC/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
+class Users(models.Model):
     username = models.CharField(max_length=20)
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=20)
@@ -9,6 +9,3 @@ class User(models.Model):
     last_name = models.CharField(max_length=20)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    
-    def __str__(self):
-        return self.username
--- AplicacaoDeMVC/users/views.py
+++ AplicacaoDeMVC/users/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from .models import User
+from .models import Users
 
 # Create your views here.
-def show_users(request):
-    users = User.objects.all()
+def show_users(request):  
+    
+    novo_user = Users.objects.create(username="GuiCotta", email="guilhermecottamr@hotmail.com", password="123456", first_name="Guilherme", last_name="Cotta", is_staff=False, is_superuser=False)
+    novo_user2 = Users.objects.create(username="CarlinhosDonato", email="carlosdonato@gmail.com", password="654321", first_name="Carlos", last_name="Donato", is_staff=False, is_superuser=False)
+    novo_user3 = Users.objects.create(username="LucasRibeiro", email="lucasribeiro@outlook.com", password="132456", first_name="Lucas", last_name="Ribeiro", is_staff=False, is_superuser=False)
+    
+    users = Users.objects.all()
+    
     return render(request, 'users/show_users.html', {'users':users})
